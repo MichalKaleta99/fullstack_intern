88f4ac9524a21d40f9cc040e77896e03d16a6e89
MichalKaleta99 <102754595+MichalKaleta99@users.noreply.github.com>
2022-07-07 23:18:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def is_even(arr):
+    if((arr[0]%2+arr[1]%2==0) or ((arr[0]%2+arr[1]%2==1) and arr[2]%2==0)):
+        return True
+    else:
+        return False
+def find_number(arr):
+    even = is_even(arr)
+    for num in arr:
+        if ((even and num%2==1) or (not even and num%2==0)):
+            print(num)
+            break
+if __name__ == "__main__":
+    find_number([166, 4, 1722, 22, 14, 15, -20])
